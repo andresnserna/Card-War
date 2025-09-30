ef87c20c84403955d3dd58e9264c441c6ef260a0
andresnserna <andresnserna@icloud.com>
2025-09-29 20:41:22 -0500

    Initial Commit

---
+++ Card War/Card_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_WarApp.swift
+//  Card War
+//
+//  Created by Andrés Serna on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Card_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
